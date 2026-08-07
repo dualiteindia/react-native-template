5d97a4c0dae7e7bc039c21776cf87629c0632652
Vaibhavi Tyagi <78082871+vaibhavityagi@users.noreply.github.com>
2026-08-07 10:54:41 +0530

    Modify NotFoundScreen content and messages
    Updated the NotFoundScreen to display a preview message and a loading note.

--- ts/template/app/+not-found.tsx
+++ ts/template/app/+not-found.tsx
@@ -6,9 +6,9 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.text}>This screen doesn't exist.</Text>
+        <Text style={styles.text}>Preview will appear here</Text>
         <Link href="/" style={styles.link}>
-          <Text>Go to home screen!</Text>
+          <Text>Please wait while Dualite is building your application</Text>
         </Link>
       </View>
     </>
